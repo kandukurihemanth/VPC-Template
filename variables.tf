8b7ea219512cdd001e06501f9c684ed07e097ed4
kandukurihemanth <105703779+kandukurihemanth@users.noreply.github.com>
2022-09-14 11:51:01 +0530

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -3,14 +3,6 @@ variable "create_vpc" {
   type        = bool
   
 }
-variable "access_key" {
-  description = "access key to be provided by user"
-  type        = string
-}
-variable "secret_key" {
-  description = "secret key to be provided by user"
-  type        = string
-}
 variable "vpc_name" {
   description = " name of VPC"
   type = string
@@ -316,426 +308,425 @@ variable "customer_gateways" {
   type        = map(map(any))
   
 }
-/*
+
 variable "enable_vpn_gateway" {
   description = "Should be true if you want to create a new VPN Gateway resource and attach it to the VPC"
   type        = bool
-  default     = false
+  
 }
 
 variable "vpn_gateway_id" {
   description = "ID of VPN Gateway to attach to the VPC"
   type        = string
-  default     = ""
+  
 }
 
 variable "amazon_side_asn" {
   description = "The Autonomous System Number (ASN) for the Amazon side of the gateway. By default the virtual private gateway is created with the current default Amazon ASN."
   type        = string
-  default     = "64512"
+  
 }
 
 variable "vpn_gateway_az" {
   description = "The Availability Zone for the VPN Gateway"
   type        = string
-  default     = null
+  
 }
 
 variable "propagate_intra_route_tables_vgw" {
   description = "Should be true if you want route table propagation"
   type        = bool
-  default     = false
+  
 }
 
 variable "propagate_private_route_tables_vgw" {
   description = "Should be true if you want route table propagation"
   type        = bool
-  default     = false
+ 
 }
 
 variable "propagate_public_route_tables_vgw" {
   description = "Should be true if you want route table propagation"
   type        = bool
-  default     = false
+  
 }
 
 variable "manage_default_route_table" {
   description = "Should be true to manage default route table"
   type        = bool
-  default     = false
+  
 }
 
 variable "default_route_table_name" {
   description = "Name to be used on the default route table"
   type        = string
-  default     = null
+ 
 }
 
 variable "default_route_table_propagating_vgws" {
   description = "List of virtual gateways for propagation"
   type        = list(string)
-  default     = []
+  
 }
 
 variable "default_route_table_routes" {
   description = "Configuration block of routes. See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/default_route_table#route"
   type        = list(map(string))
-  default     = []
+  
 }
 
 variable "default_route_table_tags" {
   description = "Additional tags for the default route table"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "tags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
-  default     = {}
 }
 
 variable "vpc_tags" {
   description = "Additional tags for the VPC"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "igw_tags" {
   description = "Additional tags for the internet gateway"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "public_subnet_tags" {
   description = "Additional tags for the public subnets"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "private_subnet_tags" {
   description = "Additional tags for the private subnets"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "outpost_subnet_tags" {
   description = "Additional tags for the outpost subnets"
   type        = map(string)
-  default     = {}
+ 
 }
 
 variable "public_route_table_tags" {
   description = "Additional tags for the public route tables"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "private_route_table_tags" {
   description = "Additional tags for the private route tables"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "database_route_table_tags" {
   description = "Additional tags for the database route tables"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "redshift_route_table_tags" {
   description = "Additional tags for the redshift route tables"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "elasticache_route_table_tags" {
   description = "Additional tags for the elasticache route tables"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "intra_route_table_tags" {
   description = "Additional tags for the intra route tables"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "database_subnet_group_name" {
   description = "Name of database subnet group"
   type        = string
-  default     = null
+  
 }
 
 variable "database_subnet_tags" {
   description = "Additional tags for the database subnets"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "database_subnet_group_tags" {
   description = "Additional tags for the database subnet group"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "redshift_subnet_tags" {
   description = "Additional tags for the redshift subnets"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "redshift_subnet_group_name" {
   description = "Name of redshift subnet group"
   type        = string
-  default     = null
+  
 }
 
 variable "redshift_subnet_group_tags" {
   description = "Additional tags for the redshift subnet group"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "elasticache_subnet_group_name" {
   description = "Name of elasticache subnet group"
   type        = string
-  default     = null
+  
 }
 
 variable "elasticache_subnet_group_tags" {
   description = "Additional tags for the elasticache subnet group"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "elasticache_subnet_tags" {
   description = "Additional tags for the elasticache subnets"
   type        = map(string)
-  default     = {}
+ 
 }
 
 variable "intra_subnet_tags" {
   description = "Additional tags for the intra subnets"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "public_acl_tags" {
   description = "Additional tags for the public subnets network ACL"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "private_acl_tags" {
   description = "Additional tags for the private subnets network ACL"
   type        = map(string)
-  default     = {}
+
 }
 
 variable "outpost_acl_tags" {
   description = "Additional tags for the outpost subnets network ACL"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "intra_acl_tags" {
   description = "Additional tags for the intra subnets network ACL"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "database_acl_tags" {
   description = "Additional tags for the database subnets network ACL"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "redshift_acl_tags" {
   description = "Additional tags for the redshift subnets network ACL"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "elasticache_acl_tags" {
   description = "Additional tags for the elasticache subnets network ACL"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "dhcp_options_tags" {
   description = "Additional tags for the DHCP option set (requires enable_dhcp_options set to true)"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "nat_gateway_tags" {
   description = "Additional tags for the NAT gateways"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "nat_eip_tags" {
   description = "Additional tags for the NAT EIP"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "customer_gateway_tags" {
   description = "Additional tags for the Customer Gateway"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "vpn_gateway_tags" {
   description = "Additional tags for the VPN gateway"
   type        = map(string)
-  default     = {}
+ 
 }
 
 variable "vpc_flow_log_tags" {
   description = "Additional tags for the VPC Flow Logs"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "vpc_flow_log_permissions_boundary" {
   description = "The ARN of the Permissions Boundary for the VPC Flow Log IAM Role"
   type        = string
-  default     = null
+  
 }
 
 variable "enable_dhcp_options" {
   description = "Should be true if you want to specify a DHCP options set with a custom domain name, DNS servers, NTP servers, netbios servers, and/or netbios server type"
   type        = bool
-  default     = false
+  
 }
 
 variable "dhcp_options_domain_name" {
   description = "Specifies DNS name for DHCP options set (requires enable_dhcp_options set to true)"
   type        = string
-  default     = ""
+  
 }
 
 variable "dhcp_options_domain_name_servers" {
   description = "Specify a list of DNS server addresses for DHCP options set, default to AWS provided (requires enable_dhcp_options set to true)"
   type        = list(string)
-  default     = ["AmazonProvidedDNS"]
+  
 }
 
 variable "dhcp_options_ntp_servers" {
   description = "Specify a list of NTP servers for DHCP options set (requires enable_dhcp_options set to true)"
   type        = list(string)
-  default     = []
+  
 }
 
 variable "dhcp_options_netbios_name_servers" {
   description = "Specify a list of netbios servers for DHCP options set (requires enable_dhcp_options set to true)"
   type        = list(string)
-  default     = []
+  
 }
 
 variable "dhcp_options_netbios_node_type" {
   description = "Specify netbios node_type for DHCP options set (requires enable_dhcp_options set to true)"
   type        = string
-  default     = ""
+  
 }
 
 variable "manage_default_vpc" {
   description = "Should be true to adopt and manage Default VPC"
   type        = bool
-  default     = false
+  
 }
 
 variable "default_vpc_name" {
   description = "Name to be used on the Default VPC"
   type        = string
-  default     = null
+  
 }
 
 variable "default_vpc_enable_dns_support" {
   description = "Should be true to enable DNS support in the Default VPC"
   type        = bool
-  default     = true
+  
 }
 
 variable "default_vpc_enable_dns_hostnames" {
   description = "Should be true to enable DNS hostnames in the Default VPC"
   type        = bool
-  default     = false
+  
 }
 
 # tflint-ignore: terraform_unused_declarations
 variable "default_vpc_enable_classiclink" {
   description = "[DEPRECATED](https://github.com/hashicorp/terraform/issues/31730) Should be true to enable ClassicLink in the Default VPC"
   type        = bool
-  default     = false
+  
 }
 
 variable "default_vpc_tags" {
   description = "Additional tags for the Default VPC"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "manage_default_network_acl" {
   description = "Should be true to adopt and manage Default Network ACL"
   type        = bool
-  default     = false
+ 
 }
 
 variable "default_network_acl_name" {
   description = "Name to be used on the Default Network ACL"
   type        = string
-  default     = null
+  
 }
 
 variable "default_network_acl_tags" {
   description = "Additional tags for the Default Network ACL"
   type        = map(string)
-  default     = {}
+  
 }
 
 variable "public_dedicated_network_acl" {
   description = "Whether to use dedicated network ACL (not default) and custom rules for public subnets"
   type        = bool
-  default     = false
+  
 }
 
 variable "private_dedicated_network_acl" {
   description = "Whether to use dedicated network ACL (not default) and custom rules for private subnets"
   type        = bool
-  default     = false
+  
 }
 
 variable "outpost_dedicated_network_acl" {
   description = "Whether to use dedicated network ACL (not default) and custom rules for outpost subnets"
   type        = bool
-  default     = false
+  
 }
 
 variable "intra_dedicated_network_acl" {
   description = "Whether to use dedicated network ACL (not default) and custom rules for intra subnets"
   type        = bool
-  default     = false
+  
 }
 
 variable "database_dedicated_network_acl" {
   description = "Whether to use dedicated network ACL (not default) and custom rules for database subnets"
   type        = bool
-  default     = false
+  
 }
 
 variable "redshift_dedicated_network_acl" {
   description = "Whether to use dedicated network ACL (not default) and custom rules for redshift subnets"
   type        = bool
-  default     = false
+ 
 }
 
 variable "elasticache_dedicated_network_acl" {
   description = "Whether to use dedicated network ACL (not default) and custom rules for elasticache subnets"
   type        = bool
-  default     = false
+  
 }
 
 variable "default_network_acl_ingress" {
@@ -1013,97 +1004,95 @@ variable "elasticache_outbound_acl_rules" {
 variable "manage_default_security_group" {
   description = "Should be true to adopt and manage default security group"
   type        = bool
-  default     = false
+  
 }
 
 variable "default_security_group_name" {
   description = "Name to be used on the default security group"
   type        = string
-  default     = null
 }
 
 variable "default_security_group_ingress" {
   description = "List of maps of ingress rules to set on the default security group"
   type        = list(map(string))
-  default     = []
+  
 }
 
 variable "enable_flow_log" {
   description = "Whether or not to enable VPC Flow Logs"
   type        = bool
-  default     = false
+  
 }
 
 variable "default_security_group_egress" {
   description = "List of maps of egress rules to set on the default security group"
   type        = list(map(string))
-  default     = []
+  
 }
 
 variable "default_security_group_tags" {
   description = "Additional tags for the default security group"
   type        = map(string)
-  default     = {}
 }
 
 variable "create_flow_log_cloudwatch_log_group" {
   description = "Whether to create CloudWatch log group for VPC Flow Logs"
   type        = bool
-  default     = false
+  
 }
 
 variable "create_flow_log_cloudwatch_iam_role" {
   description = "Whether to create IAM role for VPC Flow Logs"
   type        = bool
-  default     = false
+ 
 }
 
 variable "flow_log_traffic_type" {
   description = "The type of traffic to capture. Valid values: ACCEPT, REJECT, ALL."
   type        = string
-  default     = "ALL"
+  
 }
 
 variable "flow_log_destination_type" {
   description = "Type of flow log destination. Can be s3 or cloud-watch-logs."
   type        = string
-  default     = "cloud-watch-logs"
+ 
 }
 
 variable "flow_log_log_format" {
   description = "The fields to include in the flow log record, in the order in which they should appear."
   type        = string
-  default     = null
+  
 }
 
 variable "flow_log_destination_arn" {
   description = "The ARN of the CloudWatch log group or S3 bucket where VPC Flow Logs will be pushed. If this ARN is a S3 bucket the appropriate permissions need to be set on that bucket's policy. When create_flow_log_cloudwatch_log_group is set to false this argument must be provided."
   type        = string
-  default     = ""
+  
 }
 
 variable "flow_log_cloudwatch_iam_role_arn" {
   description = "The ARN for the IAM role that's used to post flow logs to a CloudWatch Logs log group. When flow_log_destination_arn is set to ARN of Cloudwatch Logs, this argument needs to be provided."
   type        = string
-  default     = ""
+  
 }
 
 variable "flow_log_cloudwatch_log_group_name_prefix" {
   description = "Specifies the name prefix of CloudWatch Log Group for VPC flow logs."
   type        = string
-  default     = "/aws/vpc-flow-log/"
+  
 }
 
 variable "flow_log_cloudwatch_log_group_retention_in_days" {
   description = "Specifies the number of days you want to retain log events in the specified log group for VPC flow logs."
   type        = number
-  default     = null
+  
 }
 
 variable "flow_log_cloudwatch_log_group_kms_key_id" {
   description = "The ARN of the KMS Key to use when encrypting log data for VPC flow logs."
   type        = string
-  default     = null
+ 
 }
 
 variable "flow_log_max_aggregation_interval" {
@@ -1159,11 +1148,7 @@ variable "flow_log_per_hour_partition" {
   
 }
 
-variable "putin_khuylo" {
-  description = "Do you agree that Putin doesn't respect Ukrainian sovereignty and territorial integrity? More info: https://en.wikipedia.org/wiki/Putin_khuylo!"
-  type        = bool
-  
-}*/
+
 
 
 
